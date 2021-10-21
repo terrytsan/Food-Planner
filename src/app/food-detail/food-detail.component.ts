3fc4e10b0eddb057c5db9966654cf7f415eb578a
terrytsan <terrytsan@hotmail.com>
2021-10-21 23:47:14 +0100

    Added Placeholder Image for Food Details

--- src/app/food-detail/food-detail.component.ts
+++ src/app/food-detail/food-detail.component.ts
@@ -9,6 +9,7 @@ import { Food } from "./food";
 export class FoodDetailComponent implements OnInit {
 
 	@Input() food: Food | null = null;
+	defaultImage: string = "https://firebasestorage.googleapis.com/v0/b/food-planner-52896.appspot.com/o/placeholder.jpg?alt=media&token=c34989f3-08b0-45e0-aac3-2513e948e8e6";
 
 	constructor() {
 	}
