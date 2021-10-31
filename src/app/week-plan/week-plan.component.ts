478f75b0c85e73cb7bacf102fb51bc1ef8f62f22
terrytsan <terrytsan@hotmail.com>
2021-10-31 14:45:33 +0000

    Added Ability to Change Selected Week
    - Added buttons to change the current week in week-plan-component.
    - Subscribe to add missing days now only runs once.
    - Hardcoded 25/10/21 as the earliest date.
    - Day of the week is now displayed next to date.

--- src/app/week-plan/week-plan.component.ts
+++ src/app/week-plan/week-plan.component.ts
@@ -4,6 +4,7 @@ import { Observable } from "rxjs";
 import { FoodPlan } from "../food-plan-detail/foodPlan";
 import { Timestamp } from "firebase/firestore";
 import { AngularFirestore } from "@angular/fire/compat/firestore";
+import { take } from "rxjs/operators";
 
 @Component({
 	selector: 'app-week-plan',
@@ -13,41 +14,20 @@ import { AngularFirestore } from "@angular/fire/compat/firestore";
 export class WeekPlanComponent implements OnInit {
 
 	foodPlans$: Observable<FoodPlan[]>;
+	selectedWeek: Week;
+	earliestStartingWeek = Timestamp.fromDate(new Date("25 October 2021"));		// Prevent weeks earlier than this date from being generated
 
-	constructor(afs: Firestore, public firestore: AngularFirestore) {
-		let week = this.getCurrentWeek();
+	constructor(public afs: Firestore, public firestore: AngularFirestore) {
+		this.selectedWeek = this.getCurrentWeek();
 		this.foodPlans$ = collectionData<FoodPlan>(
 			query<FoodPlan>(
 				collection(afs, 'foodPlans') as CollectionReference<FoodPlan>,
-				where('date', '>=', week.startDate),
-				where('date', '<=', week.endDate)
+				where('date', '>=', this.selectedWeek.startDate),
+				where('date', '<=', this.selectedWeek.endDate)
 			), {idField: 'id'}
 		);
 
-		this.foodPlans$.subscribe(foodPlans => {
-			if (foodPlans.length === 7) {
-				return;
-			}
-
-			let existingDates = foodPlans.map(f => f.date.toDate().getTime());		// Store as time so it can be compared
-			let datesToAdd: Timestamp[] = [];
-			let i = week.startDate.toDate();
-			for (; i <= week.endDate.toDate();) {
-				if (!existingDates.includes(i.getTime())) {
-					datesToAdd.push(Timestamp.fromDate(i));
-				}
-				i.setDate(i.getDate() + 1);
-			}
-
-			// Batch add the missing days
-			const batch = this.firestore.firestore.batch();
-			datesToAdd.forEach(date => {
-				const newPlanRef = this.firestore.firestore.collection('foodPlans').doc();
-				batch.set(newPlanRef, {date: date});
-			});
-			batch.commit();
-		});
-
+		this.addMissingDays();
 	}
 
 	ngOnInit(): void {
@@ -71,6 +51,53 @@ export class WeekPlanComponent implements OnInit {
 
 	trackById(index: number, item: any) {
 		return JSON.stringify(item);
+	}
+
+	advanceWeek(advanceDays: number) {
+		let newStartDate: Date = this.selectedWeek.startDate.toDate();
+		newStartDate.setDate(newStartDate.getDate() + advanceDays);
+
+		let newEndDate: Date = this.selectedWeek.endDate.toDate();
+		newEndDate.setDate(newEndDate.getDate() + advanceDays);
+
+		this.selectedWeek.startDate = Timestamp.fromDate(newStartDate);
+		this.selectedWeek.endDate = Timestamp.fromDate(newEndDate);
+
+		this.foodPlans$ = collectionData<FoodPlan>(
+			query<FoodPlan>(
+				collection(this.afs, 'foodPlans') as CollectionReference<FoodPlan>,
+				where('date', '>=', this.selectedWeek.startDate),
+				where('date', '<=', this.selectedWeek.endDate)
+			), {idField: 'id'}
+		);
+		this.addMissingDays();
+	}
+
+	private addMissingDays() {
+		// Only need this to run once (user can't add/remove foodPlans)
+		this.foodPlans$.pipe(take(1)).subscribe(foodPlans => {
+			if (foodPlans.length >= 7) {
+				return;
+			}
+
+			let existingDates = foodPlans.map(f => f.date.toDate().getTime());		// Store as time so it can be compared
+			let datesToAdd: Timestamp[] = [];
+			let i = this.selectedWeek.startDate.toDate();
+			for (; i <= this.selectedWeek.endDate.toDate();) {
+				if (!existingDates.includes(i.getTime())) {
+					datesToAdd.push(Timestamp.fromDate(i));
+				}
+				i.setDate(i.getDate() + 1);
+			}
+
+			// Batch add the missing days
+			const batch = this.firestore.firestore.batch();
+			datesToAdd.forEach(date => {
+				const newPlanRef = this.firestore.firestore.collection('foodPlans').doc();
+				batch.set(newPlanRef, {date: date});
+			});
+			batch.commit();
+		});
 	}
 }
 
