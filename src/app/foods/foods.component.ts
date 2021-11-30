380bc2077ce24c6498e7920a617ee23637995b01
terrytsan <terrytsan@hotmail.com>
2021-11-30 22:16:45 +0000

    Foods are now ordered by name

--- src/app/foods/foods.component.ts
+++ src/app/foods/foods.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { collection, collectionData, CollectionReference, Firestore, query } from "@angular/fire/firestore";
+import { collection, collectionData, CollectionReference, Firestore, orderBy, query } from "@angular/fire/firestore";
 import { Observable } from "rxjs";
 import { Food } from "../food-detail/food";
 import { MatDialog } from "@angular/material/dialog";
@@ -15,8 +15,10 @@ export class FoodsComponent implements OnInit {
 	foods$: Observable<Food[]>;
 
 	constructor(firestore: Firestore, public dialog: MatDialog) {
-		this.foods$ = collectionData<Food>(query<Food>(
-				collection(firestore, 'foods') as CollectionReference<Food>
+		this.foods$ = collectionData<Food>(
+			query<Food>(
+				collection(firestore, 'foods') as CollectionReference<Food>,
+				orderBy('name')
 			), {idField: 'id'}
 		);
 	}
