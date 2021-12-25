496ec7fda9ebf9678c83d73f6e11deb8a6fbaf51
Terry <terrytsan@hotmail.com>
2021-12-25 23:03:06 +0000

    Added Ability to Add New Catalogue Items
    - Can't add images yet.

--- src/app/catalogue-item-details/catalogue-item-details.component.ts
+++ src/app/catalogue-item-details/catalogue-item-details.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from "@angular/router";
-import { deleteDoc, doc, Firestore, onSnapshot, updateDoc } from "@angular/fire/firestore";
+import { addDoc, collection, deleteDoc, doc, Firestore, onSnapshot, updateDoc } from "@angular/fire/firestore";
 import { CatalogueItem } from "../catalogue-item/catalogueItem";
+import { Timestamp } from "firebase/firestore";
 
 @Component({
 	selector: 'app-catalogue-item-details',
@@ -12,22 +13,37 @@ export class CatalogueItemDetailsComponent implements OnInit {
 
 	id: string;
 	isEditing: boolean = false;
+	isAdding: boolean = false;
 	defaultImage: string = "assets/images/placeholder.jpg";
 	catalogueItem: CatalogueItem;
 
 	constructor(private route: ActivatedRoute, private afs: Firestore, private router: Router) {
 		this.id = this.route.snapshot.params['id'];
 
-		onSnapshot(doc(this.afs, "catalogueItems", this.id), (doc) => {
-			this.catalogueItem = doc.data() as CatalogueItem;
-		});
+		if (this.id === "new") {
+			this.catalogueItem = <CatalogueItem>{
+				name: '',
+				description: '',
+				storePurchased: '',
+				image: '',
+				status: 'neutral'
+			};
+			this.isAdding = this.isEditing = true;
+		} else {
+			onSnapshot(doc(this.afs, "catalogueItems", this.id), (doc) => {
+				this.catalogueItem = doc.data() as CatalogueItem;
+			});
+		}
 	}
 
 	ngOnInit(): void {
 	}
 
 	toggleEditing() {
-		if (this.isEditing) {
+		if (this.isAdding) {
+			this.addCatalogueItem();
+			this.isAdding = false;
+		} else if (this.isEditing) {
 			this.updateCatalogueItem();
 		}
 		this.isEditing = !this.isEditing;
@@ -64,5 +80,14 @@ export class CatalogueItemDetailsComponent implements OnInit {
 		const catalogueItemRef = doc(this.afs, 'catalogueItems', this.id);
 		await deleteDoc(catalogueItemRef);
 		await this.router.navigate(['/foodCatalogue']);
+	}
+
+	async addCatalogueItem() {
+		await addDoc(collection(this.afs, 'catalogueItems'), {
+			name: this.catalogueItem.name,
+			description: this.catalogueItem.description,
+			storePurchased: this.catalogueItem.storePurchased,
+			dateAdded: Timestamp.fromDate(new Date())
+		});
 	}
 }
