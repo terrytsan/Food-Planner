c61550e0391a73c50eb7b1066a3ca0e082c4401c
terrytsan <terrytsan@hotmail.com>
2022-01-24 23:01:13 +0000

    Added Duplicate FoodPlans

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import {
+	collection,
+	collectionData,
+	CollectionReference,
+	deleteDoc,
+	doc,
+	Firestore,
+	orderBy,
+	query
+} from "@angular/fire/firestore";
+import { FoodPlan } from "../food-plan-preview/foodPlan";
+import { map } from "rxjs/operators";
+import { Observable } from "rxjs";
 
 @Component({
 	selector: 'app-admin',
@@ -7,10 +20,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-	constructor() {
+	duplicateFoodPlans$: Observable<FoodPlan[]>;
+	emptyDuplicateFoodPlans$: Observable<FoodPlan[]>;
+
+	constructor(private afs: Firestore) {
+		this.initDuplicateFoodPlan();
 	}
 
 	ngOnInit(): void {
 	}
 
+	initDuplicateFoodPlan() {
+		this.duplicateFoodPlans$ = collectionData<FoodPlan>(
+			query<FoodPlan>(
+				collection(this.afs, 'foodPlans') as CollectionReference<FoodPlan>,
+				orderBy('date')
+			), {idField: 'id'}
+		).pipe(map(foodPlans => {
+			const lookup = foodPlans.reduce((a, e) => {
+				a.set(e.date.seconds, (a.get(e.date.seconds) ?? 0) + 1);
+				return a;
+			}, new Map());
+
+			return foodPlans.filter(e => lookup.get(e.date.seconds) > 1);
+		}));
+
+		this.emptyDuplicateFoodPlans$ = this.duplicateFoodPlans$.pipe(map(foodPlans => {
+			return foodPlans.filter(e => !e.foods);
+		}));
+	}
+
+	async deleteFoodPlan(id: string) {
+		const foodPlanRef = doc(this.afs, 'foodPlans', id);
+		await deleteDoc(foodPlanRef);
+	}
 }
